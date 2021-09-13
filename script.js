6b6aa2963913de202bc103b581df4676303845b9
hamzaig <hamzaig@yahoo.com>
2021-09-13 21:23:44 +0500

    Change Architecture to Object Oriented Paradigm

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 'use strict';
-
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,51 +9,64 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class App {
+    #map;
+    #mapEvent;
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+    constructor() {
+        this._getPosition();
+        form.addEventListener("submit", this._newWorkout.bind(this));
+        inputType.addEventListener("change", this._toggleElevationField.bind(this))
+    }
+
+    _getPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+                console.log("Could not get your position");
+            });
+        }
+    }
+
+    _loadMap(position) {
         const { latitude, longitude } = position.coords;
-
         const coords = [latitude, longitude];
-
-        map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, 13);
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(this.#map);
 
-        map.on("click", function (mapE) {
-            mapEvent = mapE;
-            form.classList.remove("hidden");
-            inputDistance.focus();
-        });
-    }, function () {
-        console.log("Could not get your position");
-    });
+        this.#map.on("click", this._showForm.bind(this));
+    }
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+    }
+
+    _toggleElevationField() {
+        inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+        inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+    }
+
+    _newWorkout(e) {
+        e.preventDefault();
+        inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = "";
+        const { lat, lng } = this.#mapEvent.latlng;
+
+        L.marker([lat, lng]).addTo(this.#map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: "running-popup",
+                })
+            )
+            .setPopupContent("Hamza")
+            .openPopup();
+    }
 }
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = "";
-
-    const { lat, lng } = mapEvent.latlng;
-    L.marker([lat, lng]).addTo(map)
-        .bindPopup(
-            L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: "running-popup",
-            })
-        )
-        .setPopupContent("Hamza")
-        .openPopup();
-});
-
-
-inputType.addEventListener("change", () => {
-    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-})
+const app = new App();
